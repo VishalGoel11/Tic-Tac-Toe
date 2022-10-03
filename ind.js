bd2847aba1bb30aed36ad733ef9bca8ff6a5969b
Vishal Goel <vishugoel2812@gmail.com>
2022-10-04 01:27:35 +0530

    javascript added but logic for winner is not written

--- ind.js
+++ ind.js
@@ -1,2 +1,19 @@
-var box=document.getElementsByClassName("innerbox1");
-console.log(box[0])
+function turn(turn1) {
+    if (turn1 === true)
+        return "x";
+    return "o";
+}
+var flag = 0;
+var box = document.getElementsByClassName("innerbox");
+Array.from(box).forEach((element, index, box) => {
+    box[index].addEventListener("click", () => {
+        if (box[index].innerText === "") {
+            if (flag % 2 == 0) {
+                element.innerText = turn(true);
+            } else {
+                element.innerText = turn(false);
+            }
+            flag++;
+        }
+    });
+});
