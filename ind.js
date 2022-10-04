ccb07d2480e013b90ebfbe5a8823874edb7a4d1d
Vishal Goel <vishugoel2812@gmail.com>
2022-10-04 13:02:01 +0530

    logic for draw is updated in the javascipt file

--- ind.js
+++ ind.js
@@ -4,56 +4,85 @@ function turn(turn1) {
         return "x";
     return "o";
 }
+var audio = new Audio("87631833.mp3");
 var box = document.getElementsByClassName("innerbox");
+var whiwin= document.getElementById("whowin");
+var flag2=false;
 function win(winner){
     if((box[0].innerText==="x" && box[1].innerText==="x"&& box[2].innerText==="x")|| (box[0].innerText==="o" && box[1].innerText==="o"&& box[2].innerText==="o")){
         box[0].style.backgroundColor="red";
         box[1].style.backgroundColor="red";
         box[2].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
     }
     if((box[3].innerText==="x" && box[4].innerText==="x"&& box[5].innerText==="x")||(box[3].innerText==="o" && box[4].innerText==="o"&& box[5].innerText==="o")){
         box[3].style.backgroundColor="red";
         box[4].style.backgroundColor="red";
         box[5].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[6].innerText==="x" && box[7].innerText==="x"&& box[8].innerText==="x")||(box[6].innerText==="o" && box[7].innerText==="o"&& box[8].innerText==="o")){
         box[6].style.backgroundColor="red";
         box[7].style.backgroundColor="red";
         box[8].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[0].innerText==="x" && box[3].innerText==="x"&& box[6].innerText==="x")|| (box[0].innerText==="o" && box[3].innerText==="o"&& box[6].innerText==="o")){
         box[0].style.backgroundColor="red";
         box[3].style.backgroundColor="red";
         box[6].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[1].innerText==="x" && box[4].innerText==="x"&& box[7].innerText==="x")||(box[1].innerText==="o" && box[4].innerText==="o"&& box[7].innerText==="o")){
         box[1].style.backgroundColor="red";
         box[4].style.backgroundColor="red";
         box[7].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[2].innerText==="x" && box[5].innerText==="x"&& box[8].innerText==="x")||(box[2].innerText==="o" && box[5].innerText==="o"&& box[8].innerText==="o")){
         box[2].style.backgroundColor="red";
         box[5].style.backgroundColor="red";
         box[8].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[0].innerText==="x" && box[4].innerText==="x"&& box[8].innerText==="x")|| (box[0].innerText==="o" && box[4].innerText==="o"&& box[8].innerText==="o")){
         box[0].style.backgroundColor="red";
         box[4].style.backgroundColor="red";
         box[8].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
+
     }
     if((box[2].innerText==="x" && box[4].innerText==="x"&& box[6].innerText==="x")||(box[2].innerText==="o" && box[4].innerText==="o"&& box[6].innerText==="o")){
         box[2].style.backgroundColor="red";
         box[4].style.backgroundColor="red";
         box[6].style.backgroundColor="red";
-        alert(winner+" wins");
+        flag2=true;
+        whiwin.innerHTML="<h1> "+winner+" wins</h1>";
+        audio.play();
     }
+}
+function draw()
+{
+    whiwin.innerHTML="<h1>It's a draw .</h1>";
 }
 var flag = 0;
 Array.from(box).forEach((element, index, box) => {
@@ -75,6 +104,9 @@ Array.from(box).forEach((element, index, box) => {
             document.getElementById("playerTurn").innerText="o";
             }
             win(text);
+            if(flag2==false&& box[0].innerText!="" && box[1].innerText!="" && box[2].innerText!=""&& box[3].innerText!="" && box[4].innerText!="" && box[5].innerText!=""&& box[6].innerText!=""&& box[7].innerText!=""&& box[8].innerText!=""){
+                draw();
+            }
             flag++;
         }
     });
@@ -83,6 +115,9 @@ function resetgame(){
     document.getElementById("playerTurn").innerText="x";
     Array.from(box).forEach((element,index)=>{
             box[index].innerText="";
+            box[index].style.backgroundColor="white";
+            flag=0;
+            whiwin.innerHTML="";
             // console.log("vishu")
 
     })
